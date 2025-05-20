8a874f01d8c58b1061852578cbf5fbf9661b24ad
JoelK <jkolaw00@gmail.here>
2025-05-20 17:33:14 -0400

    schemas done

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,56 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from schemas import (
+    WatchlistCreate,
+    WatchlistOut,
+    NoteCreate,
+    NoteOut,
+    UserCreate,
+    UserOut,
+    PlaylistCreate,
+    PlaylistOut,
+)
 
 
 DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/anime"
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(bind=engine)
+
+
+def get_watchlists() -> list[WatchlistOut]:
+    db = sessionLocal()
+    db_watchlists = db.query(DBWatchlist).all()
+
+
+    watchlists = []
+
+    for db_watchlist in db_watchlists:
+        watchlists.append(
+            WatchlistOut(
+                watchlist_id=db_watchlist.watchlist_id,
+                title=db_watchlist.title,
+                img_url=db_watchlist.img_url,
+                genre=db_watchlist.genre,
+                user_id=db_watchlist.user_id
+            )
+        )
+    db.close()
+    return watchlists
+
+
+
+
+def create_user(user: UserCreate) -> UserOut
+    db = sessionLocal()
+    new_user = DBUser(**model_dump)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    user = UserOut(
+    id = new_user.id
+    username = new_user.username
+    password = new_user.password
+    email = new_user.email)
+    return user`
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,2 +1,57 @@
 from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String
+
+
+class UserBase(BaseModel):
+    username: str
+    pasword: str
+    email: str
+
+
+class NoteBase(BaseModel):
+    note: str
+    user_id: int
+
+
+class WatchlistBase(BaseModel):
+    title: str
+    img_url: str
+    genre: str
+    user_id: int
+
+
+class PlaylistBase(BaseModel):
+    user_id: int
+    song_id: int
+    anime_id: int
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    id: int
+
+
+class PlaylistCreate(PlaylistBase):
+    pass
+
+
+class PlaylistOut(PlaylistBase):
+    playlist_id: int
+
+
+class WatchlistOut(WatchlistBase):
+    watchlist_id: int
+
+
+class WatchlistCreate(WatchlistBase):
+    pass
+
+
+class NoteCreate(NoteBase):
+    pass
+
+
+class NoteOut(NoteBase):
+    note_id: int
