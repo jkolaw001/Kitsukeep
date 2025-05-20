d7e05ae3b9e27bd650a0d166741d274155a76e84
Connor Graves <connor.graves94@gmail.com>
2025-05-20 15:00:13 -0600

    started making DB models

--- backend/db_models.py
+++ backend/db_models.py
@@ -1,5 +1,18 @@
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Date, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base, mapped_column, Mapped
 
 
-BASE = declarative_base()
+Base = declarative_base()
+
+
+class DBUser(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+
+
+class DBPlaylist(Base):
+    __table
