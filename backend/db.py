0fef98ab7a42edee88bcc6db297f53a5490772bf
JoelK <jkolaw00@gmail.here>
2025-05-20 15:32:08 -0400

    changed the database

--- backend/db.py
+++ backend/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/game"
+DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/anime"
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(bind=engine)
