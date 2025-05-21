dcfd9a4d9436d579e13eb49e67672f74a1ce59be
michael_flores <maf1113@yahoo.com>
2025-05-21 12:46:09 -0500

    db.py files

--- backend/db.py
+++ backend/db.py
@@ -113,3 +113,80 @@ def create_playlist(playlist: PlaylistCreate) -> PlaylistOut:
     )
     db.close()
     return result
+
+
+def get_user(user_id: int) -> UserOut | None:
+    db = sessionLocal()
+    user = db.query(DBUser).filter(DBUser.id == user_id).first()
+    db.close()
+    return user
+
+
+def get_playlist(playlist_id: int) -> PlaylistOut | None:
+    db = sessionLocal()
+    playlist = db.query(DBPlaylist).filter(DBPlaylist.id == playlist_id).first()
+    db.close()
+    return playlist
+
+
+def get_watchlist(watchlist_id: int) -> WatchlistOut | None:
+    db = sessionLocal()
+    watchlist = db.query(DBWatchlist).filter(DBWatchlist.id == watchlist_id).first()
+    db.close()
+    return watchlist
+
+
+def get_note(note_id: int) -> NoteOut | None:
+    db = sessionLocal()
+    note = db.query(DBNotes).filter(DBNotes.id == note_id).first()
+    db.close()
+    return note
+
+
+def get_all_playlists() -> list[PlaylistOut]:
+    db = sessionLocal()
+    playlist_model = db.query(DBPlaylist).all()
+    playlist_list = []
+    for playlist in playlist_model:
+        playlist_list.append(playlist)
+    db.close()
+    return playlist_list
+
+
+def get_all_watchlists() -> list[WatchlistOut]:
+    db = sessionLocal()
+    watchlist_model = db.query(DBWatchlist).all()
+    watchlist_list = []
+    for watchlist in watchlist_model:
+        watchlist_list.append(watchlist)
+    db.close()
+    return watchlist_list
+
+
+def get_all_notes() -> list[NoteOut]:
+    db = sessionLocal()
+    notes_model = db.query(DBNotes).all()
+    notes_list = []
+    for notes in notes_model:
+        notes_list.append(notes)
+    db.close()
+    return notes_list
+
+
+def get_all_users() -> list[UserOut]:
+    db = sessionLocal()
+    user_model = db.query(DBUser).all()
+    user_list = []
+    for user in user_model:
+        user_list.append(user)
+    db.close()
+    return user_list
+
+
+def delete_user(user_id: int):
+    db = sessionLocal()
+    user_model = db.query(DBUser).filter(DBUser.id == user_id).first()
+    db.delete(user_model)
+    db.commit()
+    db.close()
+    return {"detail": f"{user_id} has been deleted owo"}
