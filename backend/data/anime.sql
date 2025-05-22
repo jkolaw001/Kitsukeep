f01dd88025ac0995c6c6416a85ad30e05494624c
Connor Graves <connor.graves94@gmail.com>
2025-05-22 12:22:54 -0600

    updated users table in anime.sql to reflect db model

--- backend/data/anime.sql
+++ backend/data/anime.sql
@@ -9,9 +9,10 @@ drop table if exists songs;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    session_token VARCHAR(255) NOT NULL,
+    session_expires_at: TIMESTAMP NOT NULL
 );
 
 -- ANIME TABLE
