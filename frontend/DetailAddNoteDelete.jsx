b363b04a7df076e7e9fad6de206aa4b91f7a20c1
maf1113 <maf1113@yahoo.com>
2025-05-23 18:11:41 +0000

    Updated to have no anime year

--- frontend/DetailAddNoteDelete.jsx
+++ frontend/DetailAddNoteDelete.jsx
@@ -8,7 +8,6 @@ export default function AnimeDetailsInWatchlist({}) {
        <>
        <img className="detail-image" src="https://placehold.co/125x200" />
       <h2>"anime.title"</h2>
-      <p>"anime.year"</p>
       <p>"anime.description"</p>
       <p>"anime.rating"</p>
       <p>"anime.genre"</p>
