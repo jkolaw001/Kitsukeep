7018c49b669d8f36ec4c9bbb64d90360665ebcff
JoelK <jkolaw00@gmail.here>
2025-05-23 14:57:29 -0400

    Watchlist page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './pages/Home'
+import HomePage from './pages/Home.jsx'
+import { Routes, Route } from 'react-router-dom'
+import WatchlistPage from './pages/Watchlist.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <HomePage />
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/Watchlist' element={<WatchlistPage />} />
+      </Routes>
+
+
   )
 }
 
--- frontend/src/pages/Watchlist.jsx
+++ frontend/src/pages/Watchlist.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import "./Home.css";
+import "./Watchlist.css";
 import { Link } from "react-router-dom";
-import WatchlistPage from "./Watchlist.jsx";
 
-function HomePage() {
+function WatchlistPage() {
   return (
     <>
       <div className="nav-container" id="navContainer">
@@ -12,8 +11,13 @@ function HomePage() {
             <Link to='/' className="main-logo">
               Kitsukeep
             </Link>
-            <Link to="/Watchlist" className="menu-button">Watchlist</Link>
-            <a href="#" className="menu-button">Playlists</a>
+            <Link to="/Watchlist" className="menu-button">
+              Watchlist
+            </Link>
+
+            <a href="#" className="menu-button">
+              Playlists
+            </a>
           </div>
           <div className="right-section">
             <a href="#">Some Stuff</a>
@@ -35,110 +39,73 @@ function HomePage() {
           </div>
         </div>
       </div>
-
-      <div className="content">
-        <div className="carousel-section">
-          <h1 className="carousel-title"> Carousel Here</h1>
-          <p className="carousel-subtitle">Some Animes images here</p>
-        </div>
-        <button className="carousel-button">#</button>
-        <p>Your site content goes here.</p>
+      <div className="watchlist-container">
+        <h1>| My Watchlist |</h1>
       </div>
-
       <div className="anime-cards">
         <div className="card">
           <h3 className="card-title">Anime 1</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 2</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 3</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 4</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 5</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 6</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 7</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 8</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 9</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 10</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 11</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 12</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 13</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 14</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
         <div className="card">
           <h3 className="card-title">Anime 1</h3>
-          <p className="card-description">
-            Anime name and a few info
-          </p>
+          <p className="card-description">Anime name and a few info</p>
         </div>
       </div>
     </>
   );
 }
 
-export default HomePage;
+export default WatchlistPage;
